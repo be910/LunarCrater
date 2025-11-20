9b481239fc2587be33cdb45f6f607769f97848fe
be910 <minhanh91022@gmail.com>
2025-11-19 22:41:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ function drawMarePolygon() {
 
 // Load Craters
 function loadCraters(file) {
-    const url = "https://media.githubusercontent.com/media/be_910/LunarCrater_FinProject/main/erased_craters_mare.geojson";
+    const url = "https://media.githubusercontent.com/media/be_910/LunarCrater/main/erased_craters_mare.geojson";
     
     d3.json(url).then(data => {
         allCraters = data.features;
